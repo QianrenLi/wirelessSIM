ee6cb038128ff0acf1340c4b17789733eb9133a4
641103903 <57205576+641103903@users.noreply.github.com>
2023-09-05 21:17:29 +0800

    Add files via upload

--- encoder.py
+++ encoder.py
@@ -1,6 +1,9 @@
+import logging
 import math
 import operator
 import random
+import warnings
+
 import numpy as np
 
 from packets import packets
@@ -14,7 +17,7 @@ class encoder():
         '''
         pkt_num = len(srcPkts.packets)
         pkt_ind = [i for i in range(pkt_num)]
-        c, delta = 0.05, 0.05
+        c, delta = 0.025, 0.05
         neighbor_num = self.degreeSelection(c, delta, pkt_num)
 
         neighbor_ids = random.sample(pkt_ind, neighbor_num)
@@ -32,26 +35,33 @@ class encoder():
 
     def degreeSelection(self, c, delta, k):
         mu = self.robustSoliton(c, delta, k)
-        d_set = [i for i in range(1, len(mu))]
-        d = np.random.choice(d_set, p=mu)
+        d_set = [i for i in range(1, len(mu) + 1)]
+        d = np.random.choice(d_set, p=mu) + 1
+        print(d)
         return d
 
     def robustSoliton(self, c, delta, k):
         rho = [0] * k
         rho[0] = 1 / k
-        for d in range(2, k):
-            rho[d] = 1 / (d * (d - 1))
+        for d in range(2, k + 1):
+            rho[d - 1] = 1 / (d * (d - 1))
 
         R = c * math.log(k / delta) * math.sqrt(k)
-        d_max = math.ceil(k / R)
-        tau = [0] * d_max
-        for d in range(d_max - 1):
-            tau[d] = R / (d * k)
-        tau[-1] = R * math.log(R / delta)
+        d_max = math.floor(k / R)
+        if d_max > k:
+            logging.error("Parameter setting for robust soliton distribution is improper. Please change the setting (c, delta).")
+            exit()
+        tau = [0] * k
+        for d in range(1, d_max):
+            try:
+                tau[d - 1] = R / (d * k)
+            except:
+                print(d)
+        tau[d_max - 1] = R * math.log(R / delta) / k
 
         beta = sum(rho + tau)
-        mu = [0] * d_max
-        for d in range(d_max):
+        mu = [0] * k
+        for d in range(k):
             mu[d] = (rho[d] + tau[d]) / beta
 
         return mu
